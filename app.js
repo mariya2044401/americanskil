2ac494c45b76b8ed5ba35488e238972832ac9445
Mariya Khan <jiyarehman80@gmail.com>
2024-08-12 02:52:52 +0500

    project
    project

---
+++ app.js
@@ -0,0 +1,15 @@
+// // Initialization for ES Users
+// import { Ripple, initMDB } from "mdb-ui-kit";
+
+// initMDB({ Ripple });
+
+// showw more
+let hide1 = document.querySelectorAll("btn2")
+function hide(){
+    if(hide1.style.display != "none"){
+       hide1.style.display ="none"
+    }
+    else{
+       hide1.style.display ="block"
+    }
+}
